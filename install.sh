25f9eaa758f44cad9f7325a947a7068374c38de9
Fabian Yamaguchi <fabs@shiftleft.io>
2020-11-01 15:08:11 +0100

    Run schema extender

--- install.sh
+++ install.sh
@@ -3,9 +3,10 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-readonly JOERN_INSTALL=~/bin/joern/
-readonly JAR_INSTALL_DIR=${JOERN_INSTALL}/joern-cli/lib/
-readonly SCHEMA_DIR=src/main/schema/
+readonly JOERN_INSTALL=~/bin/joern/joern-cli
+readonly JAR_INSTALL_DIR=${JOERN_INSTALL}/lib/
+
+readonly SCHEMA_SRC_DIR=src/main/schema/
 
 echo "Examining Joern installation..."
 
@@ -21,3 +22,8 @@ sbt stage
 echo "Installing jars into: ${JAR_INSTALL_DIR}"
 cp target/universal/stage/lib/io.joern.joern-sample-extension-*.jar ${JAR_INSTALL_DIR}
 cp target/universal/stage/lib/org.eclipse.jgit.org.eclipse.jgit* ${JAR_INSTALL_DIR}
+
+echo "Adapting CPG schema"
+pushd $JOERN_INSTALL
+./schema-extender.sh
+popd
