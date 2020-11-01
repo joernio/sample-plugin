5c67fd173a96e029a76a7547b7c9569cb064881e
Fabian Yamaguchi <fabs@shiftleft.io>
2020-11-01 15:02:04 +0100

    Check for joern installation in install script

--- install.sh
+++ install.sh
@@ -1,5 +1,23 @@
-readonly JOERN_INSTALL=~/bin/joern/
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -o nounset
 
+readonly JOERN_INSTALL=~/bin/joern/
+readonly JAR_INSTALL_DIR=${JOERN_INSTALL}/joern-cli/lib/
+readonly SCHEMA_DIR=src/main/schema/
+
+echo "Examining Joern installation..."
+
+if [ ! -d "${JOERN_INSTALL}" ]; then
+    echo "Cannot find Joern installation at ${JOERN_INSTALL}"
+    echo "Please install Joern first"
+    exit
+fi
+
+echo "Compiling (sbt stage)..."
 sbt stage
-cp target/universal/stage/lib/io.joern.joern-sample-extension-*.jar ${JOERN_INSTALL}/joern-cli/lib/
-cp target/universal/stage/lib/org.eclipse.jgit.org.eclipse.jgit* ${JOERN_INSTALL}/joern-cli/lib/
+
+echo "Installing jars into: ${JAR_INSTALL_DIR}"
+cp target/universal/stage/lib/io.joern.joern-sample-extension-*.jar ${JAR_INSTALL_DIR}
+cp target/universal/stage/lib/org.eclipse.jgit.org.eclipse.jgit* ${JAR_INSTALL_DIR}
